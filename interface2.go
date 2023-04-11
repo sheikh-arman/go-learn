9a142e33a7cc9c5d87caa6e8b4b630f08cc65f45
sheikh-arman <arman.cse4.bu@gmail.com>
2023-04-11 11:12:29 +0600

    interface

---
+++ interface2.go
@@ -0,0 +1,14 @@
+import (
+	"fmt"
+)
+
+func PrintAll(vals []interface{}) {
+	for _, val := range vals {
+		fmt.Println(val)
+	}
+}
+
+func main() {
+	names := []string{"stanley", "david", "oscar"}
+	PrintAll(names)
+}
